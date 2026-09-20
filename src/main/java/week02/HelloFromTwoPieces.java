ba99cbe394ec57739959cbb2d37e6fd231b5dda9
İbrahim Sağdöken <i.sagdoken@gmail.com>
2026-09-20 18:28:16 +0300

    hello world

---
+++ src/main/java/week02/HelloFromTwoPieces.java
@@ -0,0 +1,15 @@
+package week02;
+
+public class HelloFromTwoPieces {
+    public static void main(String[] args) {
+        System.out.println(a() + b());
+    }
+
+    static String a() {
+        return "hello";
+    }
+
+    static String b() {
+        return "world";
+    }
+}
